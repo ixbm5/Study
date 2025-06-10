9529aee14b71d0f6d0c18d09a23ba37a9a3a1b69
saeed alghamdi <ixbm@saeeds-MacBook-Pro.local>
2025-06-10 11:04:57 +0300

    done the first project

---
+++ Projects_notD/cacl.py
@@ -0,0 +1,13 @@
+print("plesee enter two numbers:")
+x= int(input("the first num"))
+sin = input("plesse enter the opriton:")
+y = int(input("scond number:"))
+
+if (sin =="+"):
+    print (x+y)
+elif(sin=='-'):
+    print(x-y)
+elif(sin =='x'):
+    print(x*y)
+else:
+    print("wroing input")
